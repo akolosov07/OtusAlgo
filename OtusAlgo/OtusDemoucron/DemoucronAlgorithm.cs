caff3cd10924940c7c35e6f71a86a12a6d72359b
Александр Колосов <a.kolosov@fenix-ipoteka.ru>
2023-05-12 15:53:06 +0300

    алгоритм Демукрона

---
+++ OtusAlgo/OtusDemoucron/DemoucronAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusDemoucron
+{
+    public class DemoucronAlgorithm
+    {
+        public List<List<int>> GetLevels(int[][] graph)
+        {
+            int[] sumArray = GetSumArray(graph);
+
+            List<List<int>> levelList = new List<List<int>>();
+
+
+            while(sumArray.Where(i => i > 0).ToList().Count > 0)
+            {
+                List<int> level = new List<int>();
+                for (int i = 0; i < sumArray.Length; i++)
+                {
+                    if (sumArray[i] == 0)
+                    {
+                        level.Add(i);
+                        sumArray[i] = -1;
+                    }
+                }
+
+                if (level.Count > 0)
+                {
+                    for (int i = 0; i < level.Count; i++)
+                    {
+                        int[] line = graph[level[i]];
+
+                        for (int j = 0; j < line.Length; j++)
+                        {
+                            if (sumArray[j] > 0)
+                                sumArray[j] = sumArray[j] - line[j];
+                        }
+                    }
+                }
+
+
+                if (level.Count > 0) levelList.Add(level);
+            }
+
+            
+            return levelList;
+        }
+
+        private int[] GetSumArray(int[][] graph)
+        {
+            int[] sumArray = new int[graph.Length];
+
+            for (int i = 0; i < sumArray.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < graph.Length; j++)
+                {
+                    sum += graph[j][i];
+                }
+                sumArray[i] = sum;
+            }
+
+            return sumArray;
+        }
+    }
+}
+
---
+++ OtusAlgo/OtusDemoucron/Program.cs
@@ -0,0 +1,24 @@
+﻿using OtusDemoucron;
+
+int[] list1A = new int[8] { 0, 1, 0, 0, 0, 0, 0, 0 };
+int[] list2A = new int[8] { 0, 0, 0, 0, 1, 0, 0, 0 };
+int[] list3C = new int[8] { 0, 0, 0, 1, 0, 0, 0, 0 };
+int[] list4D = new int[8] { 1, 1, 0, 0, 1, 1, 0, 0 };
+int[] list5E = new int[8] { 0, 0, 0, 0, 0, 0, 1, 0 };
+int[] list6F = new int[8] { 0, 0, 0, 0, 0, 0, 0, 1 };
+int[] list7G = new int[8] { 0, 0, 0, 0, 0, 0, 0, 1 };
+int[] list8H = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+int[][] graph = new int[][]
+    {
+        list1A, list2A, list3C, list4D, list5E, list6F, list7G, list8H
+    };
+
+
+DemoucronAlgorithm demoucronAlgorithm = new DemoucronAlgorithm();
+var vertexList = demoucronAlgorithm.GetLevels(graph);
+
+
+
+
+
