402dd278c9d6bc733d80ef005e4b06a5ea5c9703
Александр Колосов <a.kolosov@fenix-ipoteka.ru>
2023-05-12 22:31:55 +0300

    kruskal

---
+++ OtusAlgo/OtusKruskal/Program.cs
@@ -0,0 +1,16 @@
+﻿using OtusKruskal;
+
+Kruskal k = new Kruskal(@"4
+6
+1 2 1
+2 3 3
+3 4 2
+4 1 5
+1 3 4
+2 4 6");
+
+k.BuildSpanningTree();
+Console.WriteLine("Cost: " + k.Cost);
+k.DisplayInfo();
+Console.WriteLine("Press any key...");
+Console.ReadKey();
