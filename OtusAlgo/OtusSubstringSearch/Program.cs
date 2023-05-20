29d2b78fc4d672e6d13decd2cbc99bb8394eb15d
Александр Колосов <a.kolosov@fenix-ipoteka.ru>
2023-05-21 00:31:10 +0300

    Алгоритм Бойера-Мура

---
+++ OtusAlgo/OtusSubstringSearch/Program.cs
@@ -0,0 +1,13 @@
+﻿using OtusSubstringSearch;
+
+//SearchString searchString = new SearchString();
+//string text = "STRONGSIRING";
+//string mask = "RING";
+//int res = searchString.SearchFullScan(text, mask);
+//Console.WriteLine(res);
+
+SearchString searchString = new SearchString();
+string text = "STRONGSIRING";
+string mask = "RING";
+int res = searchString.SearchBMH(text, mask);
+Console.WriteLine(res);
---
+++ OtusAlgo/OtusSubstringSearch/Search.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusSubstringSearch
+{
+    public class SearchString
+    {
+        public int SearchFullScan(string text, string mask)
+        {
+            int t = 0;
+            while (t <= text.Length - mask.Length)
+            {
+                int m = 0;
+                while (m < mask.Length && text[t + m] == mask[m])
+                    m++;
+                if (m == mask.Length)
+                    return t;
+                t++;
+            }
+            return -1;
+        }
+
+        public int SearchBMH(string text, string mask)
+        {
+            int[] shift = CreateShift(mask);
+            int t = 0;
+            while (t <= text.Length - mask.Length)
+            {
+                int m = mask.Length - 1;
+                while (m >= 0 && text[t + m] == mask[m])
+                    m--;
+                if (m < 0)
+                    return t;
+                t += shift[text[t + mask.Length - 1]];
+            }
+            return -1;
+        }
+
+        private int[] CreateShift(string mask)
+        {
+            int[] shift = new int[128];
+            for (int i = 0; i < shift.Length; i++)
+                shift[i] = mask.Length;
+            for (int m = 0; m < mask.Length - 1; m++)
+                shift[mask[m]] = mask.Length - m - 1;
+            return shift;
+        }
+    }
+}
